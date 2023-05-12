6ae91369e128cb247e8d6134f482e6c073d88a08
Mary Scott <Mary.P.Scott@warwick.ac.uk>
2023-05-12 17:04:09 +0100

    Add files via upload

--- schulman_Tsample_kld.py
+++ schulman_Tsample_kld.py
@@ -1,6 +1,5 @@
 import torch
 import torch.distributions as dis
-from collections import Counter
 
 p = dis.Normal(loc=0, scale=1)
 q = dis.Normal(loc=0.1, scale=1)
@@ -10,8 +9,6 @@ print("true", truekl)
 x = q.sample(sample_shape=(10_000_000,))
 y = x.clone().detach()
 z = torch.cat((x, y))
-freq = Counter(z)
-print(freq.most_common())
 
 logr = (p.log_prob(z) - q.log_prob(z))
 k3 = ((logr.exp() - 1) - logr)
