12f89d1bcc596e0897ecbe34f895920331a464d0
Mary Scott <Mary.P.Scott@warwick.ac.uk>
2023-05-19 16:00:29 +0100

    Add files via upload

--- schulman_Tsample_kld.py
+++ schulman_Tsample_kld.py
@@ -7,7 +7,7 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 p = dis.Normal(loc=0, scale=1)
-q = dis.Normal(loc=0.1, scale=1)
+q = dis.Normal(loc=1, scale=1)
 truekl = dis.kl_divergence(p, q)
 print("true", truekl)
 
@@ -19,10 +19,14 @@ z = torch.cat((x, y))
 # skip Prio step for now
 logr = (p.log_prob(z) - q.log_prob(z))
 k3 = ((logr.exp() - 1) - logr)
-print((k3.mean() - truekl) / truekl, k3.std() / truekl)
+print(f'Approx vs true KLD (no noise): {(k3.mean() - truekl) / truekl, k3.std() / truekl}')
 
 # add Laplace noise with eps = 1 (full distribution)
 a, b1 = 0, log(2)
 noise = tfp.distributions.Laplace(loc=a, scale=b1)
 k3noise = k3 + noise.sample(sample_shape=20_000_000)
-print((np.mean(k3noise) - truekl) / truekl, np.std(k3noise) / truekl)
+print(f'Approx KLD (noise vs no noise): {(np.mean(k3noise) - k3.mean()) / k3.mean(), np.std(k3noise) / k3.std()}')
+
+trueklnoise = truekl + noise.sample(sample_shape=20_000_000)
+print(f'Approx vs true KLD (with noise): {(np.mean(k3noise) - np.mean(trueklnoise)) / np.mean(trueklnoise), np.std(k3noise) / np.std(trueklnoise)}')
+print(f'True KLD (noise vs no noise): {(np.mean(trueklnoise) - truekl) / truekl, np.std(trueklnoise) / truekl}')
