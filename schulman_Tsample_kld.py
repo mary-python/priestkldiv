732320c2b0bcfe2ec8faf1192b6d14534b6d2cb7
Mary Scott <Mary.P.Scott@warwick.ac.uk>
2023-05-30 16:21:44 +0100

    Add files via upload

--- schulman_Tsample_kld.py
+++ schulman_Tsample_kld.py
@@ -12,7 +12,8 @@ truekl = dis.kl_divergence(p, q)
 print("true", truekl)
 
 # sample T = 10 million points
-x = q.sample(sample_shape=(10_000_000,))
+T = 10_000_000
+x = q.sample(sample_shape=(T,))
 y = x.clone().detach()
 z = torch.cat((x, y))
 
@@ -21,13 +22,18 @@ logr = (p.log_prob(z) - q.log_prob(z))
 k3 = ((logr.exp() - 1) - logr)
 print(f'Approx vs true KLD (no noise): {(k3.mean() - truekl) / truekl, k3.std() / truekl}')
 
-# add Laplace noise with eps = 1 (full distribution)
-a, b1 = 0, log(2)
+# add Laplace or Gaussian noise with parameter(s) eps (and dta)
+eps = 1
+dta = 1
+a = 0
+b1 = log(2)
+b2 = 2*((log(1.25))/dta)*b1
 noise = tfp.distributions.Laplace(loc=a, scale=b1)
-k3noise = k3 + noise.sample(sample_shape=20_000_000)
+# noise = tfp.distributions.Normal(loc=a, scale=b2)
+k3noise = k3 + (noise.sample(sample_shape=2*T))/eps
 print(f'Approx KLD (noise vs no noise): {(np.mean(k3noise) - k3.mean()) / k3.mean(), np.std(k3noise) / k3.std()}')
 
 # additional comparisons with true KLD
-trueklnoise = truekl + noise.sample(sample_shape=20_000_000)
+trueklnoise = truekl + noise.sample(sample_shape=2*T)
 print(f'Approx vs true KLD (with noise): {(np.mean(k3noise) - np.mean(trueklnoise)) / np.mean(trueklnoise), np.std(k3noise) / np.std(trueklnoise)}')
 print(f'True KLD (noise vs no noise): {(np.mean(trueklnoise) - truekl) / truekl, np.std(trueklnoise) / truekl}')
