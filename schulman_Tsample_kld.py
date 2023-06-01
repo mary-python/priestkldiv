e74c7a4ce73ca713e408b891147a0ecdcb001433
Mary Scott <Mary.P.Scott@warwick.ac.uk>
2023-06-01 18:16:26 +0100

    Add files via upload

--- schulman_Tsample_kld.py
+++ schulman_Tsample_kld.py
@@ -11,14 +11,23 @@ q = dis.Normal(loc=0, scale=1)
 truekl = dis.kl_divergence(p, q)
 print("true", truekl)
 
-# sample T = 10 million points
+# sample T points
 T = 10_000_000
-x = q.sample(sample_shape=(T,))
-y = x.clone().detach()
-z = torch.cat((x, y))
+
+# round to 2 d.p., find indices of and eliminate unique values
+qSample = q.sample(sample_shape=(T,))
+qRound = torch.round(qSample, decimals=2)
+qUnique = torch.unique(qRound, return_counts=True)
+qIndices = (qUnique[1] == 1).nonzero().flatten()
+qUniqueIndices = qUnique[0][qIndices]
+
+for i in qUniqueIndices:
+    qRound = qRound[qRound != i]
+
+qT = torch.numel(qRound)
 
 # skip Prio step for now
-logr = (p.log_prob(z) - q.log_prob(z))
+logr = (p.log_prob(qRound) - q.log_prob(qRound))
 k3 = ((logr.exp() - 1) - logr)
 print(f'Approx vs true KLD (no noise): {(k3.mean() - truekl) / truekl, k3.std() / truekl}')
 
@@ -30,10 +39,10 @@ b1 = log(2)
 b2 = 2*((log(1.25))/dta)*b1
 noise = tfp.distributions.Laplace(loc=a, scale=b1)
 # noise = tfp.distributions.Normal(loc=a, scale=b2)
-k3noise = k3 + (noise.sample(sample_shape=2*T))/eps
+k3noise = k3 + (noise.sample(sample_shape=qT))/eps
 print(f'Approx KLD (noise vs no noise): {(np.mean(k3noise) - k3.mean()) / k3.mean(), np.std(k3noise) / k3.std()}')
 
 # additional comparisons with true KLD
-trueklnoise = truekl + noise.sample(sample_shape=2*T)
+trueklnoise = truekl + noise.sample(sample_shape=qT)
 print(f'Approx vs true KLD (with noise): {(np.mean(k3noise) - np.mean(trueklnoise)) / np.mean(trueklnoise), np.std(k3noise) / np.std(trueklnoise)}')
 print(f'True KLD (noise vs no noise): {(np.mean(trueklnoise) - truekl) / truekl, np.std(trueklnoise) / truekl}')
