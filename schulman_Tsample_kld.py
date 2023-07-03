053f3d6df18e0a21bbe4a701ca938d0bc0480715
Mary Scott <Mary.P.Scott@warwick.ac.uk>
2023-07-03 16:37:41 +0100

    Add files via upload

--- schulman_Tsample_kld.py
+++ schulman_Tsample_kld.py
@@ -1,9 +1,10 @@
 import torch
 import torch.distributions as dis
 from math import log
+import tensorflow as tf
+tf.random.set_seed(12)
 import tensorflow_probability as tfp
 import numpy as np
-from scipy.stats import laplace, norm
 import matplotlib.pyplot as plt
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -31,30 +32,37 @@ qT = torch.numel(qRound)
 # skip Prio step for now
 logr = (p.log_prob(qRound) - q.log_prob(qRound))
 k3 = ((logr.exp() - 1) - logr)
-print(f'Approx vs true KLD (no noise): {(k3.mean() - truekl) / truekl, k3.std() / truekl}')
 
 # add Laplace or Gaussian noise with parameter(s) eps (and dta)
-eps = 0.1
+epsset = [0.01, 0.025, 0.05, 0.1, 0.2, 0.4, 0.8, 1, 1.5, 2, 3, 4]
 dta = 0.1
 a = 0
 b1 = log(2)
 b2 = 2*((log(1.25))/dta)*b1
-noise = tfp.distributions.Laplace(loc=a, scale=b1)
-# noise = tfp.distributions.Normal(loc=a, scale=b2)
-k3noise = k3 + (noise.sample(sample_shape=qT))/eps
-print(f'Approx KLD (noise vs no noise): {(np.mean(k3noise) - k3.mean()) / k3.mean(), np.std(k3noise) / k3.std()}')
+noise1 = tfp.distributions.Laplace(loc=a, scale=b1)
+noise2 = tfp.distributions.Normal(loc=a, scale=b2)
 
-# additional comparisons with true KLD
-trueklnoise = truekl + noise.sample(sample_shape=qT)
-print(f'Approx vs true KLD (with noise): {(np.mean(k3noise) - np.mean(trueklnoise)) / np.mean(trueklnoise), np.std(k3noise) / np.std(trueklnoise)}')
-print(f'True KLD (noise vs no noise): {(np.mean(trueklnoise) - truekl) / truekl, np.std(trueklnoise) / truekl}')
+k3noise1 = list()
+k3noise2 = list()
+KLDest1 = list()
+KLDest2 = list()
 
-rv1 = laplace(a, b1)
-rv2 = norm(a, b1)
-dist1 = np.linspace(-2, np.minimum(rv1.dist.b, 2))
-dist2 = np.linspace(-2, np.minimum(rv2.dist.b, 2))
-plot1 = plt.plot(dist1, rv1.pdf(dist1), label = f"Lap dist: \u03B5={eps}")
-plot2 = plt.plot(dist2, rv2.pdf(dist2), label = f"N dist: \u03B5={eps}, \u03B4={dta}")
+for eps in epsset:
 
-plt.legend(loc="upper left")
+    for i in range(0, 10):
+        k3noise1.append(k3 + (noise1.sample(sample_shape=qT, seed=12))/eps)
+        k3noise2.append(k3 + (noise2.sample(sample_shape=qT, seed=12))/eps)
+
+    average1 = sum(k3noise1) / len(k3noise1)
+    average2 = sum(k3noise2) / len(k3noise2)
+    KLDest1.append(abs(np.mean(average1) - truekl))
+    KLDest2.append(abs(np.mean(average2) - truekl))
+
+plot1 = plt.plot(epsset, KLDest1, label = f"Laplace dist")
+plot2 = plt.plot(epsset, KLDest2, label = f"Gaussian dist")
+
+plt.title("Effect of epsilon on noisy estimate of KLD")
+plt.xlabel("Value of epsilon")
+plt.ylabel("Difference in KLD")
+plt.legend(loc="best")
 plt.show()
