b07c1e0cf2df183f148384cec92a8f38cdb4a399
Mary Scott <Mary.P.Scott@warwick.ac.uk>
2023-09-11 16:51:33 +0100

    Add files via upload

--- schulman_Tsample_kld_T.py
+++ schulman_Tsample_kld_T.py
@@ -38,17 +38,17 @@ qT2 = np.zeros(C)
 qOrderedT1 = np.zeros(C)
 qOrderedT2 = np.zeros(C)
 logr = np.zeros(C)
-Ologr = np.zeros(C)
+oLogr = np.zeros(C)
 k3 = np.zeros(C)
-Ok3 = np.zeros(C)
+oK3 = np.zeros(C)
 
 # store T before C (clients)
 Tset = np.array([5_000, 10_000, 25_000, 50_000, 100_000, 250_000, 500_000, 1_000_000, 2_500_000, 5_000_000])
 L = np.size(Tset)
 KLDest1 = np.zeros((L, C))
 KLDest2 = np.zeros((L, C))
-OKLDest1 = np.zeros((L, C))
-OKLDest2 = np.zeros((L, C))
+oKLDest1 = np.zeros((L, C))
+oKLDest2 = np.zeros((L, C))
 TCount = 0
 
 for T in Tset:
@@ -56,8 +56,8 @@ for T in Tset:
     # create separate numpy arrays for sampled and ordered cases
     k3noise1 = np.zeros(L)
     k3noise2 = np.zeros(L)
-    Ok3noise1 = np.zeros(L)
-    Ok3noise2 = np.zeros(L)
+    oK3noise1 = np.zeros(L)
+    oK3noise2 = np.zeros(L)
 
     # carry out 10 repeats
     for k in range(0, 10):
@@ -72,57 +72,63 @@ for T in Tset:
         for i in qUniqueIndices:
             qRound = qRound[qRound != i]
 
-        # order the pre-processed sample
+        # order the pre-processed sample and separate into + and - values
         qOrderedRound = torch.sort(qRound)
+        qPositiveRound = torch.where(qRound >= 0, qRound, -qRound)
+        qNegativeRound = torch.where(qRound < 0, qRound, -qRound)
 
         for j in range(0, C):
 
-            # sample from the pre-processed sample
-            qClientSample[j] = qRound.sample(sample_shape=(N,))
-            qT1[j] = torch.numel(qClientSample[j])
+            # even clients get positive values, odd clients get negative values
+            if (j % 2) == 0:
+                qClientSample[j] = torch.multinomial(qPositiveRound, N, False)
+            else:
+                qClientSample[j] = -torch.multinomial(qNegativeRound, N, False)
+
+            qT1[j] = torch.numel(torch.from_numpy(qClientSample[j]))
             qClientSample[j] = qClientSample[j][abs(int(qT1 - 0.98*T)):]
-            qT2[j] = torch.numel(qClientSample[j])
+            qT2[j] = torch.numel(torch.from_numpy(qClientSample[j]))
 
             # each client gets 500 points in order from ordered pre-processed sample
-            qOrderedClientSample[j] = qOrderedRound[(500*j):(500*(j+1))]
-            qOrderedT1[j] = torch.numel(qOrderedClientSample[j])
+            qOrderedClientSample[j] = qOrderedRound[0][(500*j):(500*(j+1))]
+            qOrderedT1[j] = torch.numel(torch.from_numpy(qOrderedClientSample[j]))
             qOrderedClientSample[j] = qOrderedClientSample[j][abs(int(qOrderedT1 - 0.98*T)):]
-            qT2[j] = torch.numel(qOrderedClientSample[j])
+            qT2[j] = torch.numel(torch.from_numpy(qOrderedClientSample[j]))
     
             # skip Prio step for now
-            logr[j] = (p.log_prob(qClientSample[j]) - q.log_prob(qClientSample[j]))
-            Ologr[j] = (p.log_prob(qOrderedClientSample[j]) - q.log_prob(qOrderedClientSample[j]))
+            logr[j] = (p.log_prob(torch.from_numpy(qClientSample[j])) - q.log_prob(torch.from_numpy(qClientSample[j])))
+            oLogr[j] = (p.log_prob(torch.from_numpy(qOrderedClientSample[j])) - q.log_prob(torch.from_numpy(qOrderedClientSample[j])))
             k3[j] = ((logr[j].exp() - 1) - logr[j])
-            Ok3[j] = ((Ologr[j].exp() - 1) - Ologr[j])
+            oK3[j] = ((oLogr[j].exp() - 1) - oLogr[j])
         
             # add Laplace and Gaussian noise with parameter(s) eps (and dta)
             k3noise1[k] = k3 + (noise1.sample(sample_shape=qT2[j], seed=12)/eps)
             k3noise2[k] = k3 + (noise2.sample(sample_shape=qT2[j], seed=12)/eps)
-            Ok3noise1[k] = Ok3 + (noise1.sample(sample_shape=qOrderedT2[j], seed=12)/eps)
-            Ok3noise2[k] = Ok3 + (noise2.sample(sample_shape=qOrderedT2[j], seed=12)/eps)
+            oK3noise1[k] = oK3 + (noise1.sample(sample_shape=qOrderedT2[j], seed=12)/eps)
+            oK3noise2[k] = oK3 + (noise2.sample(sample_shape=qOrderedT2[j], seed=12)/eps)
 
         average1 = np.sum(k3noise1) / np.size(k3noise1)
         average2 = np.sum(k3noise2) / np.size(k3noise2)
-        Oaverage1 = np.sum(Ok3noise1) / np.size(Ok3noise1)
-        Oaverage2 = np.sum(Ok3noise2) / np.size(Ok3noise2)
+        oAverage1 = np.sum(oK3noise1) / np.size(oK3noise1)
+        oAverage2 = np.sum(oK3noise2) / np.size(oK3noise2)
 
         # compare with true KLD
         KLDest1[TCount, C] = abs(np.mean(average1) - truekl)
         KLDest2[TCount, C] = abs(np.mean(average2) - truekl)
-        OKLDest1[TCount, C] = abs(np.mean(Oaverage1) - truekl)
-        OKLDest2[TCount, C] = abs(np.mean(Oaverage2) - truekl)
+        oKLDest1[TCount, C] = abs(np.mean(oAverage1) - truekl)
+        oKLDest2[TCount, C] = abs(np.mean(oAverage2) - truekl)
         TCount = TCount + 1
 
 # compute mean of KLD for particular T across all clients
 KLDmean1 = np.mean(KLDest1, axis = 1)
 KLDmean2 = np.mean(KLDest2, axis = 1)
-OKLDmean1 = np.mean(OKLDest1, axis = 1)
-OKLDmean2 = np.mean(OKLDest2, axis = 1)
+oKLDmean1 = np.mean(oKLDest1, axis = 1)
+oKLDmean2 = np.mean(oKLDest2, axis = 1)
 
 plot1 = plt.plot(Tset, KLDest1, label = f"Laplace (sampled)")
 plot2 = plt.plot(Tset, KLDest2, label = f"Gaussian (sampled)")
-plot3 = plt.plot(Tset, OKLDest1, label = f"Laplace (ordered)")
-plot4 = plt.plot(Tset, OKLDest2, label = f"Gaussian (ordered)")
+plot3 = plt.plot(Tset, oKLDest1, label = f"Laplace (ordered)")
+plot4 = plt.plot(Tset, oKLDest2, label = f"Gaussian (ordered)")
 
 plt.title("Effect of T on noisy estimate of KLD")
 plt.xlabel("Value of T")
--- schulman_Tsample_kld_eps.py
+++ schulman_Tsample_kld_eps.py
@@ -28,8 +28,10 @@ qUniqueIndices = qUnique[0][qIndices]
 for i in qUniqueIndices:
     qRound = qRound[qRound != i]
 
-# order the pre-processed sample
+# order the pre-processed sample and separate into + and - values
 qOrderedRound = torch.sort(qRound)
+qPositiveRound = torch.where(qRound >= 0, qRound, -qRound)
+qNegativeRound = torch.where(qRound < 0, qRound, -qRound)
 
 # 10-100K clients each with a few hundred points
 C = 10000
@@ -41,9 +43,9 @@ qOrderedClientSample = np.zeros((C, N))
 qT = np.zeros(C)
 qOrderedT = np.zeros(C)
 logr = np.zeros(C)
-Ologr = np.zeros(C)
+oLogr = np.zeros(C)
 k3 = np.zeros(C)
-Ok3 = np.zeros(C)
+oK3 = np.zeros(C)
 
 # parameters for the addition of Laplace and Gaussian noise
 epsset = np.array([0.01, 0.025, 0.05, 0.1, 0.2, 0.4, 0.8, 1, 1.5, 2, 3, 4])
@@ -58,23 +60,27 @@ noise2 = tfp.distributions.Normal(loc=a, scale=b2)
 L = np.size(epsset)
 KLDest1 = np.zeros((C, L))
 KLDest2 = np.zeros((C, L))
-OKLDest1 = np.zeros((C, L))
-OKLDest2 = np.zeros((C, L))
+oKLDest1 = np.zeros((C, L))
+oKLDest2 = np.zeros((C, L))
 
 for j in range(0, C):
 
-    # sample from the pre-processed sample
-    qClientSample[j] = qRound.sample(sample_shape=(N,))
-    qT[j] = torch.numel(qClientSample[j])
+    # even clients get positive values, odd clients get negative values
+    if (j % 2) == 0:
+        qClientSample[j] = torch.multinomial(qPositiveRound, N, False)
+    else:
+        qClientSample[j] = -torch.multinomial(qNegativeRound, N, False)
+
+    qT[j] = torch.numel(torch.from_numpy(qClientSample[j]))
 
     # each client gets 500 points in order from ordered pre-processed sample
-    qOrderedClientSample[j] = qOrderedRound[(500*j):(500*(j+1))]
-    qOrderedT[j] = torch.numel(qOrderedClientSample[j])
+    qOrderedClientSample[j] = qOrderedRound[0][500*j:500*(j+1)]
+    qOrderedT[j] = torch.numel(torch.from_numpy(qOrderedClientSample[j]))
 
     # skip Prio step for now
-    logr[j] = (p.log_prob(qClientSample[j]) - q.log_prob(qClientSample[j]))
-    Ologr[j] = (p.log_prob(qOrderedClientSample[j]) - q.log_prob(qOrderedClientSample[j]))
-    Ok3[j] = ((Ologr[j].exp() - 1) - Ologr[j])
+    logr[j] = (p.log_prob(torch.from_numpy(qClientSample[j])) - q.log_prob(torch.from_numpy(qClientSample[j])))
+    oLogr[j] = (p.log_prob(torch.from_numpy(qOrderedClientSample[j])) - q.log_prob(torch.from_numpy(qOrderedClientSample[j])))
+    oK3[j] = ((oLogr[j].exp() - 1) - oLogr[j])
     epsCount = 0
 
     for eps in epsset:
@@ -82,38 +88,38 @@ for j in range(0, C):
         # create separate numpy arrays for sampled and ordered cases
         k3noise1 = np.zeros(L)
         k3noise2 = np.zeros(L)
-        Ok3noise1 = np.zeros(L)
-        Ok3noise2 = np.zeros(L)
+        oK3noise1 = np.zeros(L)
+        oK3noise2 = np.zeros(L)
 
         # add average of 10 possible noise terms
         for k in range(0, 10):
             k3noise1[k] = k3 + (noise1.sample(sample_shape=qT[j], seed=12))/eps
             k3noise2[k] = k3 + (noise2.sample(sample_shape=qT[j], seed=12))/eps
-            Ok3noise1[k] = Ok3 + (noise1.sample(sample_shape=qOrderedT[j], seed=12))/eps
-            Ok3noise2[k] = Ok3 + (noise2.sample(sample_shape=qOrderedT[j], seed=12))/eps
+            oK3noise1[k] = oK3 + (noise1.sample(sample_shape=qOrderedT[j], seed=12))/eps
+            oK3noise2[k] = oK3 + (noise2.sample(sample_shape=qOrderedT[j], seed=12))/eps
 
         average1 = np.sum(k3noise1) / np.size(k3noise1)
         average2 = np.sum(k3noise2) / np.size(k3noise2)
-        Oaverage1 = np.sum(Ok3noise1) / np.size(Ok3noise1)
-        Oaverage2 = np.sum(Ok3noise2) / np.size(Ok3noise2)
+        oAverage1 = np.sum(oK3noise1) / np.size(oK3noise1)
+        oAverage2 = np.sum(oK3noise2) / np.size(oK3noise2)
 
         # compare with true KLD
         KLDest1[C, epsCount] = abs(np.mean(average1) - truekl)
         KLDest2[C, epsCount] = abs(np.mean(average2) - truekl)
-        OKLDest1[C, epsCount] = abs(np.mean(Oaverage1) - truekl)
-        OKLDest2[C, epsCount] = abs(np.mean(Oaverage2) - truekl)
+        oKLDest1[C, epsCount] = abs(np.mean(oAverage1) - truekl)
+        oKLDest2[C, epsCount] = abs(np.mean(oAverage2) - truekl)
         epsCount = epsCount + 1
 
 # compute mean of KLD for particular epsilon across all clients
 KLDmean1 = np.mean(KLDest1, axis = 0)
 KLDmean2 = np.mean(KLDest2, axis = 0)
-OKLDmean1 = np.mean(OKLDest1, axis = 0)
-OKLDmean2 = np.mean(OKLDest2, axis = 0)
+oKLDmean1 = np.mean(oKLDest1, axis = 0)
+oKLDmean2 = np.mean(oKLDest2, axis = 0)
 
 plot1 = plt.plot(epsset, KLDmean1, label = f"Laplace (sampled)")
 plot2 = plt.plot(epsset, KLDmean2, label = f"Gaussian (sampled)")
-plot3 = plt.plot(epsset, OKLDmean1, label = f"Lapplace (ordered)")
-plot4 = plt.plot(epsset, OKLDmean2, label = f"Gaussian (ordered)")
+plot3 = plt.plot(epsset, oKLDmean1, label = f"Lapplace (ordered)")
+plot4 = plt.plot(epsset, oKLDmean2, label = f"Gaussian (ordered)")
 
 plt.title("Effect of epsilon on noisy estimate of KLD")
 plt.xlabel("Value of epsilon")
