bafdc5d4475f6521a11e9b313aa441f3d604efc8
Mary Scott <Mary.P.Scott@warwick.ac.uk>
2023-10-05 16:49:56 +0100

    Add files via upload

--- femnist_hdf5.py
+++ femnist_hdf5.py
@@ -6,50 +6,41 @@ import os
 # https://blade6570.github.io/soumyatripathy/hdf5_blog.html
 # ENABLES IMAGES TO BE STORED IN A HDF5 FILE WHILE MAINTAINING FOLDERS AND SUBFOLDERS
 
-basePath = './data/by_class'
-savePath = './data/test.hdf5'
-imgPath = './data/by_class/30/hsf_0/hsf_0_00000.png'
-
-# print size of test image
-print('image size: %d bytes' %os.path.getsize(imgPath))
+basePath = '.\\Sayan Biswas\\data\\by_class'
+# savePath = '.\\Sayan Biswas\\data\\femnist_digits.hdf5'
+savePath = '.\\Sayan Biswas\\data\\test.hdf5'
 
 # open the file in append mode
 hf = h5py.File(savePath, 'a')
+numberFiles = 747260
+print(f"\nConverting directory to hdf5 format...\n")
 
-# open test image as python binary
-with open(imgPath, 'rb') as img:
-    binaryData = img.read()
+from alive_progress import alive_bar
+with alive_bar(numberFiles) as bar:
 
-# create numpy array storing python binary
-binaryDataNp = np.asarray(binaryData)
+    # read all the folders
+    for i in os.listdir(basePath):
+        name = os.path.join(basePath, i)
 
-# save test image and close file
-dset = hf.create_dataset('default', data = binaryDataNp)
-hf.close()
-print('hdf5 file size: %d bytes'%os.path.getsize(savePath))
-
-# read all the folders
-for i in os.listdir(basePath):
-    name = os.path.join(basePath, i)
-    group = hf.create_group(name)
-
-    # read all the folders inside the folders
-    for j in os.listdir(name):
-        track = os.path.join(name, j)
-        subgroup = group.create_group(j)
+        # read all the folders inside the folders
+        for j in os.listdir(name):
+            track = os.path.join(name, j)
     
-        # find all images in the subfolders
-        for k in os.listdir(track):
-            imagePath = os.path.join(track, k)
+            # find all images in the subfolders
+            for k in os.listdir(track):
+                imagePath = os.path.join(track, k)
 
-            # open images as python binary
-            with open(imagePath, 'rb') as image:
-                binaryData = image.read()
+                # open images as python binary
+                with open(imagePath, 'rb') as image:
+                    binaryData = image.read()
             
-            # create numpy array storing python binary
-            binaryDataNp = np.asarray(binaryData)
+                # create numpy array storing python binary
+                binaryDataNp = np.asarray(binaryData)
 
-            # save all images in the current subfolder
-            dset = subgroup.create_dataset(k, data = binaryDataNp)
+                # TESTING: save all images without folders or subfolders
+                # AVOID DUPLICATE NAME: dset
+                dset = hf.create_dataset(k, data = binaryDataNp)
+                bar()
 
 hf.close()
+print(f"\nDone.\n")
--- femnist_test.py
+++ femnist_test.py
@@ -1,19 +1,48 @@
 import h5py
 import numpy as np
+# from PIL import Image
+# import io
 import matplotlib.pyplot as plt
 import torch
+
+# FROM HDF5 BLOG: "HDF5 FILE FOR LARGE IMAGE DATASETS" BY SOUMYA TRIPATHY
+# https://blade6570.github.io/soumyatripathy/hdf5_blog.html
+# ENABLES IMAGES IN FOLDERS AND SUBFOLDERS TO BE EXTRACTED FROM THEIR HDF5 FILE 
+
+# data = [] # keys to access image data
+# group = [] # list all groups and subgroups in hdf5 file
+# print(f"\nExtracting keys, groups and subgroups from hdf5 file...")
+
+# def func(name, obj):
+    # if isinstance(obj, h5py.Dataset):
+        # data.append((name, obj))
+    # elif isinstance(obj, h5py.Group):
+        # group.append((name, obj))
+
+# fetch hdf5 file from current directory
+path = './Sayan Biswas/data/femnist_digits.hdf5'
+file = h5py.File(path, 'r')
+# file.visititems(func)
+
+# numberFiles = 747260
+# print(f"Extracting images from hdf5 file...\n")
+
+# from alive_progress import alive_bar
+# with alive_bar(numberFiles) as bar:
+
+    # read the image files in their proper format
+    # for j in data:
+        # kk = np.array(file[j])
+        # img = Image.open(io.BytesIO(kk)) # our image file
+        # bar()
 
 # FROM HDF5-FEMNIST BY XIAO-CHENGUANG
 # https://github.com/Xiao-Chenguang/HDF5-FEMNIST
 # ENABLES EASY ACCESS AND FAST LOADING TO THE FEMNIST DATASET FROM LEAF WITH THE HELP OF HDF5
 
-# is this path genuine or do I need to change this to my path?
-path = '.\Sayan Biswas\data'
-file = h5py.File(path, 'r')
-
-# create an list where each index stores the images and labels associated with a particular writer
-writers = sorted(file.keys)
-print(f'the dataset contains images from {len(writers)} writers') # should be 3580
+# create a list where each index stores the images and labels associated with a particular writer
+writers = sorted(file.keys())
+print(f'\nThe dataset contains images from {len(writers)} writers.') # should be 3580
 
 # store writer 0's images and labels in numpy arrays
 w0_images = file[writers[0]]['images'][:]
@@ -25,9 +54,9 @@ w0_labels_tensor = torch.from_numpy(w0_labels)
 
 # sort writer 0's images into classes (digits 0 to 9)
 classes, counts = np.unique(w0_labels, return_counts = True)
-print(f'writer 0 has {len(w0_images)} images') # should be 104
-print(f'writer 0 has {len(classes)} classes') # should be 10
-print(f'writer 0 has {counts} images per class') # should be [12 11 11 12 9 8 10 11 9 11]
+print(f'\nWriter 0 has {len(w0_images)} images') # should be 104
+print(f'Writer 0 has {len(classes)} classes') # should be 10
+print(f'Writer 0 has {counts} images per class\n') # should be [12 11 11 12 9 8 10 11 9 11]
 
 # dataset 0 stores all of writer 0's data
 d0 = file[writers[0]]
